9e007cc6bb25b78d0dee8d77b9e4490371abfd71
Gregory <gregory@Gregorys-MacBook-Pro.local>
2017-09-19 01:34:10 -0400

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Gregory Norwood, am determined to learn how to code." );
+		System.out.println( "Thursday June 1, 2017" );
+	}
+}
