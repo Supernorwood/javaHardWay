e432e699445e31fcb6eaf8d74dcd92e600cd200f
Gregory <gregory@Gregorys-MacBook-Pro.local>
2017-09-19 01:38:20 -0400

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|    Shell Gas Station   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   2017-07-16 09:27 AM  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   Gallons:      13.502 |" );
+		System.out.println( "|   Price/gallon:$ 2.643 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   Fuel total:  $ 35.69 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}	
+}	
