5b3878b36bc6bf01fb896818efe9d8eca78a215d
Gregory <gregory@Gregorys-MacBook-Pro.local>
2017-09-24 07:18:00 -0600

    add exercise 4

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,7 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\n" );
+		System.out.println( "EchoFoxtrotGolf\nHotel\nIndia\n\n" );
+	    System.out.println( "This is a test." );
+	}	
+}	
