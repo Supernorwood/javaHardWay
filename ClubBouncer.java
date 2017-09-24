1e640bc6ea653049068391a4f1789853b9be3b73
Gregory <gregory@Gregorys-MacBook-Pro.local>
2017-09-24 09:08:00 -0600

    add exercise 17

---
+++ ClubBouncer.java
@@ -0,0 +1,18 @@
+public class ClubBouncer {
+	public static void main( String[] args ) {
+		int age = 22;
+		boolean onGuestList = false;
+		double allure = 7.5;
+		String gender = "F";
+
+		if ( onGuestList || age >= 21 || (gender.equals("F") && allure >= 8) ) {
+		System.out.print("You are allowed to enter the club,");
+		}
+		if ( age > 21 && (gender.equals("F") )) {
+		System.out.println(" ma'am..");
+		}
+		else {
+		System.out.println("You are not allowed to enter the club.");
+		}
+	}
+}
